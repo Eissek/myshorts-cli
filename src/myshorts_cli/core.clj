4056232d56d82c22a1aab337cc91ed6a6c3ed444
Eissek <sane.gez@gmail.com>
2015-04-14 19:46:00 +0100

    added save shortcuts

--- src/myshorts_cli/core.clj
+++ src/myshorts_cli/core.clj
@@ -1,6 +1,7 @@
 (ns myshorts-cli.core
   (:require [clojure.string :as str]
-            [cheshire.core :refer :all])
+            [cheshire.core :refer :all]
+            [clojure.java.io :refer :all])
   (:gen-class))
 
 (defn -main
@@ -12,6 +13,30 @@
 (defn gen-uuid
   []
   (java.util.UUID/randomUUID))
+
+(defn load-shortcuts
+  [filename])
+
+(defn write-shortcut
+  [args])
+
+(def saved-file "shortcuts.json")
+
+(defn store-shortcut
+  [args]
+  (if (.exists (as-file saved-file))
+    (do
+      (let [shortcuts
+            (parse-string (slurp saved-file) true)]
+        (spit saved-file
+              (generate-string
+               (conj shortcuts args) {:pretty true})))
+      (println "Shortcut successfully stored "))
+    (do
+      (spit saved-file (generate-string [args]))
+      (println "Saved to new file")))
+  (println "DONE"))
+
 
  (defn add-shortcut
   []
@@ -35,15 +60,15 @@
         (if (empty? tags)
               (println "No tags entered.")
               (println tags))
-        (str "Added " shortcut ", " desc
-             " id: " (gen-uuid))))))
+        (store-shortcut (hash-map
+                         :id (gen-uuid)
+                         :short shortcut
+                         :tags tags)))))
 
 
-(defn load-shortcuts
-  [filename])
+(defn list-shortcuts
+  [])
 
-(defn write-shortcuts
-  [args])
 
 (defn search-shortcuts
-  [])
+  []))
