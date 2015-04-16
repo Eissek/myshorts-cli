e5fdf8b60b6cac5b674c9b1c29b5f690e3e190f8
Eissek <sane.gez@gmail.com>
2015-04-16 20:05:21 +0100

    Added arity to add-shortcut function

--- src/myshorts_cli/core.clj
+++ src/myshorts_cli/core.clj
@@ -1,14 +1,14 @@
 (ns myshorts-cli.core
   (:require [clojure.string :as str]
             [cheshire.core :refer :all]
-            [clojure.java.io :refer :all])
+            [clojure.java.io :refer :all]
+            [clojure.pprint :refer [print-table]])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (println "hi"))
+  (println "HEY HAPPY"))
 
 (defn gen-uuid
   []
@@ -45,32 +45,46 @@
 
 
  (defn add-shortcut
-  []
-  (println "Enter:")
+ ([]
+   (println "Enter:")
    (let [shortcut
          (loop [x (read-line)]
            (if (empty? x)
              (do (println "Enter shortcut")
-                     (recur (read-line)))
+                 (recur (read-line)))
              (str x)))]
-    (println "Enter shortcut Description")
-    (let [desc
-          (loop [x (read-line)]
-            (if (empty? x)
-              (do
-                (println "Enter a shortcut description ")
-                (recur (read-line)))
-              (str x)))]
-      (println "Enter tags(Optional):")
-      (let [tags (read-line)]
-        (if (empty? tags)
-              (println "No tags entered.")
-              (println tags))
-        (store-shortcut (hash-map
-                         :desc desc
-                         :id (gen-uuid)
-                         :short shortcut
-                         :tags tags))))))
+     (println "Enter shortcut Description")
+     (let [desc
+           (loop [x (read-line)]
+             (if (empty? x)
+               (do
+                 (println "Enter a shortcut description ")
+                 (recur (read-line)))
+               (str x)))]
+       (println "Enter tags(Optional):")
+       (let [tags (read-line)]
+         (if (empty? tags)
+           (println "No tags entered.")
+           (println tags))
+         (store-shortcut (hash-map
+                          :desc desc
+                          :id (gen-uuid)
+                          :short shortcut
+                          :tags tags))))))
+ ([shortcut desc & tags]
+  (store-shortcut (hash-map
+                   :desc desc
+                   :id (gen-uuid)
+                   :short shortcut
+                   :tags (if (empty? tags)
+                           (str " ")
+                          (str tags))))))
+
+
+(defn create
+  [shortcut desc & tags]
+  (str "short "
+       shortcut "desc " desc "tags " tags))
 
 
 ;; (defn list-shortcuts
